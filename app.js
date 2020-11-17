e7052180a8ab6c4999ba351d7af14b21817709d8
dklarin <danijel.klarin@gmail.com>
2020-11-17 22:24:20 +0100

    Initial upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.post('/tweeted', (request, response) => {
+    const { body } = request;
+    console.log(body.tweet);
+});
+
+app.listen(port, () => {
+    console.log(`Express api/webhook app listening at http://localhost:${port}`);
+});
